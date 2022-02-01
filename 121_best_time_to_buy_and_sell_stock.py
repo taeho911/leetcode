0a9d1be4c3868157624dd20b814523f3d96953e9
kim4485 <taeho.a.kim@rakuten-bank.co.jp>
2022-02-01 11:28:23 +0900

    121

--- 121_best_time_to_buy_and_sell_stock.py
+++ 121_best_time_to_buy_and_sell_stock.py
@@ -3,6 +3,8 @@
 # and choosing a different day in the future to sell that stock.
 # Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
 
+import sys
+
 class Solution:
     def maxProfit(self, prices):
         if len(prices) <= 1:
@@ -14,5 +16,39 @@ class Solution:
         for i in range(1, len(prices)):
             ans = max(ans, prices[i] - min_p)
             min_p = min(min_p, prices[i])
+        
+        return ans
+
+class Solution_0:
+    def maxProfit(self, prices):
+        if len(prices) <= 1:
+            return 0
+        
+        ans = 0
+        pre = sys.maxsize
+        for i in range(len(prices)-1):
+            if prices[i] < pre:
+                ans = max(ans, max(prices[i+1:]) - prices[i])
+                pre = prices[i]
+        
+        return ans
+
+class Solution_1(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        buy, sell, ans = prices[0], prices[0], 0
+        
+        for price in prices:
+            if price < buy:
+                buy = price
+                sell = price
+                continue
+            if price > sell:
+                if price - buy > ans:
+                    ans = price - buy
+                    sell = price
         
         return ans
