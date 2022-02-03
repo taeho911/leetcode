acf2548d2df126254f8369ef8107368e34a272f0
Taeho <kimdogg911@gmail.com>
2022-02-03 23:21:49 +0900

    208

--- 454_4sum_2.py
+++ 454_4sum_2.py
@@ -1,3 +1,10 @@
+'''
+Given four integer arrays nums1, nums2, nums3, and nums4 all of length n, return the number of tuples (i, j, k, l) such that:
+
+0 <= i, j, k, l < n
+nums1[i] + nums2[j] + nums3[k] + nums4[l] == 0
+'''
+
 from collections import defaultdict
 
 class Solution:
